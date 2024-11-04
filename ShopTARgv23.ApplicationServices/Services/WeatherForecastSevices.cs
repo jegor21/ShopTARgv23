04783e56dbb74114925387d7959985ed8591e584
jegor21 <jegorka2002@gmail.com>
2024-11-04 14:49:26 +0200

    AccuLocationResult

---
+++ ShopTARgv23.ApplicationServices/Services/WeatherForecastSevices.cs
@@ -0,0 +1,29 @@
+﻿using Nancy.Json;
+using ShopTARgv23.Core.Dto.WeatherDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARgv23.ApplicationServices.Services
+{
+    public class WeatherForecastSevices
+    {
+        public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
+        {
+            string accuApiKey = "oqRZLuFEsuaI2sGXWUDHviSAXGbckDF0";
+            string url = $"http://datasevice.accuweather.com//locations/v1/cities/search?apikey={accuApiKey}=Tallinn&language=en-US&details=false HTTP/1.1"
+
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                AccuLocationRootDto accuResult = new JavaScriptSerializer().Deserialize<AccuLocationRootDto>(json);
+            }
+                return dto;
+        }
+    }
+}
---
+++ ShopTARgv23.Core/Dto/WeatherDtos/AccuLocationWeatherResultDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ShopTARgv23.Core.Dto.WeatherDtos
+{
+    public class AccuLocationWeatherResultDto
+    {
+        public int Version { get; set; }
+    }
+}
