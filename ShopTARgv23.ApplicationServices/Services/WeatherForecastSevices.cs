355856e091dd094c07a84426e235f0cff2d56e53
jegor21 <jegorka2002@gmail.com>
2024-11-11 13:27:33 +0200

    AccuWeatherRootDto

--- ShopTARgv23.ApplicationServices/Services/WeatherForecastSevices.cs
+++ ShopTARgv23.ApplicationServices/Services/WeatherForecastSevices.cs
@@ -1,4 +1,5 @@
-﻿using Nancy.Json;
+﻿using Nancy;
+using Nancy.Json;
 using ShopTARgv23.Core.Dto.WeatherDtos;
 using ShopTARgv23.Core.ServiceInterface;
 using System;
@@ -17,7 +18,7 @@ namespace ShopTARgv23.ApplicationServices.Services
             string accuApiKey = "oqRZLuFEsuaI2sGXWUDHviSAXGbckDF0";
             string url = $"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={accuApiKey}&q={dto.CityName}";
 
-
+            
             using (WebClient client = new WebClient())
             {
                 string json = client.DownloadString(url);
@@ -31,7 +32,41 @@ namespace ShopTARgv23.ApplicationServices.Services
             }
 
             string urlWeather = $"http://dataservice.accuweather.com/forecasts/v1/daily/1day/{dto.CityCode}?apikey={accuApiKey}&metric=true";
-                return dto;
+               
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(urlWeather);
+                AccuWeatherRootDto weatherRootDto = new JavaScriptSerializer()
+                    .Deserialize<AccuWeatherRootDto>(json);
+
+                dto.EffectiveDate = weatherRootDto.Headline.EffectiveDate;
+                dto.EffectiveEpochDate = weatherRootDto.Headline.EffectiveEpochDate;
+                dto.Severity = weatherRootDto.Headline.Severity;
+                dto.Text = weatherRootDto.Headline.Text;
+                dto.Category = weatherRootDto.Headline.Category;
+                dto.EndDate = weatherRootDto.Headline.EndDate;
+                dto.EndEpochDate = weatherRootDto.Headline.EndEpochDate;
+                dto.MobileLink = weatherRootDto.Headline.MobileLink;
+                dto.Link = weatherRootDto.Headline.Link;
+
+                dto.Value = weatherRootDto.Maximum.Value;
+                dto.Unit = weatherRootDto.Maximum.Unit;
+                dto.UnitType = weatherRootDto.Maximum.Type;
+
+                dto.Value = weatherRootDto.Minimum.Value;
+                dto.Unit = weatherRootDto.Minimum.Unit;
+                dto.UnitType = weatherRootDto.Minimum.UnitType;
+
+                dto.Icon = weatherRootDto.Night.Icon;
+                dto.IconPhrase = weatherRootDto.Night.Icon;
+                dto.HasPrecipitation = weatherRootDto.Night.HasPrecipitation;
+
+                dto.Minimum = weatherRootDto.Temperature.Minimum;
+                dto.Maximum = weatherRootDto.Temperature.Maximum;
+
+            }
+            return dto;
         }
     }
 }
