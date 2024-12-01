fbe6ddcb748a57989a68996315b9436e91730c88
jegor21 <jegorka2002@gmail.com>
2024-12-02 01:14:23 +0200

    OpenWeatherDone

---
+++ ShopTARgv23.ApplicationServices/Services/OpenWeatherMapServices.cs
@@ -0,0 +1,43 @@
+﻿using Nancy.Json;
+using ShopTARgv23.Core.ServiceInterface;
+using ShopTARgv23.Core.Dto.OpenWeatherMap;
+using System.Net;
+
+namespace ShopTARgv23.ApplicationServices.Services
+{
+    public class OpenWeatherMapServices : IOpenWeatherMap
+    {
+        public async Task<OpenWeatherMapResultDto> OpenWeatherResult(OpenWeatherMapResultDto dto)
+        {
+            string apiKey = "ea7cbe965dbab00326be0615fca25dd7";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.City},{dto.CountryCode}&units=metric&appid={apiKey}";
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                OpenWeatherMapRoot weatherRoot = new JavaScriptSerializer().Deserialize<OpenWeatherMapRoot>(json);
+
+                dto.City = weatherRoot.Name;
+                dto.CountryCode = weatherRoot.Sys.Country;
+                dto.Temperature = weatherRoot.Main.Temp;
+                dto.FeelsLike = weatherRoot.Main.Feels_Like;
+                dto.MinTemperature = weatherRoot.Main.Temp_Min;
+                dto.MaxTemperature = weatherRoot.Main.Temp_Max;
+                dto.WeatherMain = weatherRoot.Weather[0].Main;
+                dto.WeatherDescription = weatherRoot.Weather[0].Description;
+                dto.WindSpeed = weatherRoot.Wind.Speed;
+                dto.WindDirection = weatherRoot.Wind.Deg;
+                dto.Humidity = weatherRoot.Main.Humidity;
+                dto.Pressure = weatherRoot.Main.Pressure;
+                dto.Cloudiness = weatherRoot.Clouds.All;
+
+                dto.Sunrise = DateTimeOffset.FromUnixTimeSeconds(weatherRoot.Sys.Sunrise).ToLocalTime().ToString("HH:mm");
+                dto.Sunset = DateTimeOffset.FromUnixTimeSeconds(weatherRoot.Sys.Sunset).ToLocalTime().ToString("HH:mm");
+
+            }
+
+            return dto;
+        }
+    }
+}
---
+++ ShopTARgv23.Core/ServiceInterface/IOpenWeatherMap.cs
@@ -0,0 +1,9 @@
+﻿using ShopTARgv23.Core.Dto.OpenWeatherMap;
+
+namespace ShopTARgv23.Core.ServiceInterface
+{
+    public interface IOpenWeatherMap
+    {
+        Task<OpenWeatherMapResultDto> OpenWeatherResult(OpenWeatherMapResultDto dto);
+    }
+}
