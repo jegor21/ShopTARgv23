d4a11390c399748b6558ff8ff1c3c6e4ad04b9b8
jegor21 <jegorka2002@gmail.com>
2025-01-06 15:46:04 +0200

    UserHub

--- ShopTARgv23/Program.cs
+++ ShopTARgv23/Program.cs
@@ -75,6 +75,7 @@ namespace ShopTARgv23
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapHub<ChatHub>("/chatHub");
+            app.MapHub<UserHub>("/hubs/userHub");
 
             app.Run();
         }
---
+++ ShopTARgv23/wwwroot/js/usersCount.js
@@ -0,0 +1,31 @@
+﻿
+var connectionUserCount = new signalR.HubConnectionBuilder()
+    .withAutomaticReconnect()
+    .withUrl("/hubs/userHub", signalR.HttpTransportType.WebSockets).build();
+
+
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.invoke("NewWindowLoaded", "Test").then((value) => console.log(value));
+}
+
+
+function fulfilled() {
+    console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+function rejected() {
+
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
